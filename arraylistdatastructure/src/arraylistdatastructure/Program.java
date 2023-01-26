4c486bf572b0e0eda7f5adeb269b736d9ded7876
shashank <shashank944533@gmail.com>
2023-01-26 17:19:06 +0530

    array list exe

---
+++ arraylistdatastructure/src/arraylistdatastructure/Program.java
@@ -0,0 +1,16 @@
+package arraylistdatastructure;
+
+import java.util.ArrayList;
+
+public class Program {
+public static void main(String[] args) {
+	ArrayList al = new ArrayList();
+	al.add(10);
+	al.add(20);
+	al.add(30);
+	al.add("kodnest");
+	al.add(30.34F);
+	al.add(false);
+	System.out.println(al);
+}
+}
