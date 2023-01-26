97694577577348805617fae93572ce2423beb31b
shashank <shashank944533@gmail.com>
2023-01-26 17:25:39 +0530

    Priority Queue commit

---
+++ priorityqueue/src/priorityqueue/Program.java
@@ -0,0 +1,17 @@
+package priorityqueue;
+
+import java.util.PriorityQueue;
+
+public class Program {
+public static void main(String[] args) {
+	PriorityQueue pq = new PriorityQueue();
+	pq.add(100);
+	pq.add(50);
+	pq.add(150);
+	pq.add(25);
+	pq.add(75);
+	pq.add(125);
+	pq.add(175);
+	System.out.println(pq);
+}
+}
