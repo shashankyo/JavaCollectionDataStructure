bb28c071446062cb810ae5eb70637c988f22f23c
shashank <shashank944533@gmail.com>
2023-01-26 17:27:13 +0530

    LinkedList array data structure

---
+++ linkedlistarraydatastructure/src/linkedlistarraydatastructure/Program.java
@@ -0,0 +1,16 @@
+package linkedlistarraydatastructure;
+
+import java.util.LinkedList;
+
+public class Program {
+public static void main(String[] args) {
+	LinkedList ll = new LinkedList();
+	ll.add(10);
+	ll.add(20);
+	ll.add(30);
+	ll.add("kodnest");
+	ll.add(99.5F);
+	ll.add(true);
+	System.out.println(ll);
+}
+}
