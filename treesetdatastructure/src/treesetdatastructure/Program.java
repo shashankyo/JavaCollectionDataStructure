6c4b043e43aba71c4e0ca2768a4cefc04b7ecb8e
shashank <shashank944533@gmail.com>
2023-01-26 17:24:29 +0530

    Trees Set commit

---
+++ treesetdatastructure/src/treesetdatastructure/Program.java
@@ -0,0 +1,18 @@
+package treesetdatastructure;
+
+import java.util.TreeSet;
+
+public class Program {
+public static void main(String[] args) {
+	TreeSet ts = new TreeSet();
+	ts.add(100);
+	ts.add(50);
+	ts.add(150);
+	ts.add(25);
+	ts.add(75);
+	ts.add(125);
+	ts.add(175);
+	ts.add(1);
+	System.out.println(ts);
+}
+}
