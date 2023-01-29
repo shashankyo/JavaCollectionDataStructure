5ec05c4ef6129f57951bed7e028cb36e9b5b8dc1
shashank <shashank944533@gmail.com>
2023-01-29 09:37:55 +0530

    linkedhash set commit

---
+++ linkedhashsetclassdatastructure/src/linkedhashsetclassdatastructure/Program.java
@@ -0,0 +1,19 @@
+package linkedhashsetclassdatastructure;
+
+import java.util.LinkedHashSet;
+
+public class Program {
+	public static void main(String[] args) {
+	
+	
+		LinkedHashSet hs = new LinkedHashSet();
+		hs.add(100);
+		hs.add(50);
+		hs.add(150);
+		hs.add(25);
+		hs.add(75);
+		hs.add(125);
+		hs.add(175);
+		System.out.println(hs);
+		}
+}
